7c9ac79946d5cf5587b6f361d97e863a4d46dcee
Cristi <crist@CHRISTIAN>
2025-09-19 11:03:17 +0300

    Middleware changed

--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,12 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher([
-  '/sign-in(.*)',
-  '/sign-up(.*)',
-  '/api/webhook(.*)',
-  '/api/public(.*)',
-]);
-
-export default clerkMiddleware(async (auth, req) => {
-  console.log('=== Middleware called ===');
-  console.log('URL:', req.url);
-  console.log('Auth object:', auth);
-
-  if (!isPublicRoute(req)) {
-    try {
-      await auth.protect();
-      console.log('Auth protect success!');
-    } catch (err) {
-      console.error('Auth protect failed:', err);
-      throw err; // aruncă eroarea ca să vedem exact ce se întâmplă
-    }
-  } else {
-    console.log('Public route, no auth needed');
-  }
-});
+export default clerkMiddleware()
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico).*)',
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
-};
+}
